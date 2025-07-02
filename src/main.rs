034d51963f8479cf6062e539929b4d02667d6e55
Simo-1234 <simonedalpos@gmail.com>
2025-07-02 23:46:10 +0200

    added crypto resources

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,18 @@ fn main() {
     }
 
     const PAGES: [Page; 3] = [
-        Page { folder_name: "crypto", target_file_name: "crypto" },
-        Page { folder_name: "fl-studio", target_file_name: "fl-studio" },
-        Page { folder_name: "home", target_file_name: "index" },
+        Page {
+            folder_name: "crypto",
+            target_file_name: "crypto",
+        },
+        Page {
+            folder_name: "fl-studio",
+            target_file_name: "fl-studio",
+        },
+        Page {
+            folder_name: "home",
+            target_file_name: "index",
+        },
     ];
 
     let base_filename = "base.html";
@@ -32,12 +41,15 @@ fn main() {
                     let new_filename = format!("{}.html", page.target_file_name);
                     if let Err(e) = std::fs::write(
                         &new_filename,
-                        base_contents.as_str().replace("%content%", &contents),
+                        base_contents
+                            .as_str()
+                            .replace("%title%", page.folder_name)
+                            .replace("%content%", &contents),
                     ) {
                         eprintln!("Failed to write {}: {}", new_filename, e);
                     } else {
                         println!("Created file: {}", new_filename);
-                    } 
+                    }
                 }
             }
             Err(e) => eprintln!("Failed to open {}: {}", filename, e),
